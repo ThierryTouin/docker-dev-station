ebdfa815fe23ee3b3576cdc39b2a8d4155036209
Thierry Touin <thierry.touin@gfi.fr>
2019-05-03 11:51:00 +0200

    add custom prompt

--- environnement/shell.sh
+++ environnement/shell.sh
@@ -3,5 +3,6 @@ clear
 echo "#######################################################"
 echo "###             in docker                           ###"
 echo "#######################################################"
-sudo docker container exec -it docker_dev-app_1 bash
+sudo docker container exec -it docker_dev-app_1 bash -c "echo \"export PS1='\e[0;31m[\u@\h \W]\$ \e[m '\" >> ~/.bashrc && bash"
+
 
