b2e888ecbdb15d4d42f1412226a79e4aa4da4411
Thierry Touin <thierry.touin@gfi.fr>
2020-01-23 18:12:23 +0100

    update sonar configuration

--- environnement/sonar/client/sonarClientStart.sh
+++ environnement/sonar/client/sonarClientStart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 echo start Sonar Stack
 #sudo docker-compose -f ./sonarClient.yml up -d --force-recreate
-sudo docker-compose -f ./sonarClient.yml logs -t --follow
+sudo docker-compose --verbose -f ./sonarClient.yml logs -t --follow
 
 
