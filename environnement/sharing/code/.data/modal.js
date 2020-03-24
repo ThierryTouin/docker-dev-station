4aa303c9925682cd21e81f8a06767d2b695d3578
thierry <thierrytouin@gmail.com>
2020-03-25 00:02:55 +0100

    test node filemanager for sharing system

--- environnement/sharing/code/.data/modal.js
+++
@@ -1,6 +0,0 @@
-function modal_file(file)
-{
-	document.getElementById('selected_file').innerHTML = file;
-	document.getElementById('src_file').value = file;
-	document.getElementById('files').style.display='block';
-}
