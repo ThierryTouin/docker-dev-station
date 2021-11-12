2e703fbad1eee4c8e7567cd4601352eae86c5750
Thierry Touin <thierrytouin@gmail.com>
2021-11-12 17:22:02 +0100

    portainer in latest version & change omnidb version

--- environnement/scripts/admin.sh
+++ environnement/scripts/admin.sh
@@ -1,5 +1,6 @@
 #!/bin/sh 
 echo starting portainer...
-#docker run --rm -d -p 9999:9000 -v /var/run/docker.sock:/var/run/docker.sock -v /opt/portainer:/data portainer/portainer:1.22.1
-docker run --rm -d -p 9999:9000 -v /var/run/docker.sock:/var/run/docker.sock -v /opt/portainer:/data portainer/portainer-ce:2.0.0
+docker volume create portainer_data
+docker run --rm -d -p 9999:9000 -v /var/run/docker.sock:/var/run/docker.sock -v /opt/portainer:/data portainer/portainer-ce
 echo "Admin UI started at \e[92mhttp://localhost:9999 \e[0m"
+
