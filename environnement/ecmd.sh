e23ea9fa2bfe1dd08ceba8151c14b0c17bab9e3a
Thierry Touin <thierrytouin@gmail.com>
2022-01-05 15:38:35 +0100

    add light color

--- environnement/ecmd.sh
+++ environnement/ecmd.sh
@@ -17,7 +17,7 @@ postgresql_home="./db/postgresql"
 COLOR_TITLE="\e[0;31m"
 COLOR_DEFAULT="\e[39m"
 COLOR_PARAM="\e[0;32m"
-
+COLOR_CMD="\e[1;37m"
 
 function ICJ() {
 
@@ -105,42 +105,42 @@ echo " -------------------------------------------------------------- "
 echo " -- PARAMS (env)                                             -- "
 echo " -------------------------------------------------------------- "
 echo -e ${COLOR_DEFAULT}
-echo "  startjc              : Start Java Environment"
-echo "  stopjc               : Stop Java Environment"
-echo "  injc                 : Enter in Java Container"
+echo -e "  ${COLOR_CMD}startjc${COLOR_DEFAULT}              : Start Java Environment"
+echo -e "  ${COLOR_CMD}stopjc${COLOR_DEFAULT}               : Stop Java Environment"
+echo -e "  ${COLOR_CMD}injc${COLOR_DEFAULT}                 : Enter in Java Container"
 #echo "  setEnvIonic         : Set environnement variable for Ionic"
-echo "  startwp              : Start WordPress Environment"
+echo -e "  ${COLOR_CMD}startwp${COLOR_DEFAULT}              : Start WordPress Environment"
 echo -e ${COLOR_PARAM}
 echo " -------------------------------------------------------------- "
 echo " -- PARAMS (dependencies)                                    -- "
 echo " -------------------------------------------------------------- "
 echo -e ${COLOR_DEFAULT}
-echo "  elastic              : Start Elasticsearch server"
-echo "  kibana               : Start Kibana server"
-echo "  postgresql           : Start Postgresql server"
+echo -e "  ${COLOR_CMD}elastic${COLOR_DEFAULT}              : Start Elasticsearch server"
+echo -e "  ${COLOR_CMD}kibana${COLOR_DEFAULT}               : Start Kibana server"
+echo -e "  ${COLOR_CMD}postgresql${COLOR_DEFAULT}           : Start Postgresql server"
 echo -e ${COLOR_PARAM}
 echo " -------------------------------------------------------------- "
 echo " -- PARAMS (admin)                                           -- "
 echo " -------------------------------------------------------------- "
 echo -e ${COLOR_DEFAULT}
-echo "  admin                : Start Docker Admin UI (portainer)"
-echo "  dbadmin              : Start Database Admin UI (omnidb)"
-echo "  sonar                : Start Sonar Server"
+echo -e "  ${COLOR_CMD}admin${COLOR_DEFAULT}                : Start Docker Admin UI (portainer)"
+echo -e "  ${COLOR_CMD}dbadmin${COLOR_DEFAULT}              : Start Database Admin UI (omnidb)"
+echo -e "  ${COLOR_CMD}sonar${COLOR_DEFAULT}                : Start Sonar Server"
 
 echo -e ${COLOR_PARAM}
 echo " -------------------------------------------------------------- "
 echo " -- PARAMS (tool)                                            -- "
 echo " -------------------------------------------------------------- "
 echo -e ${COLOR_DEFAULT}
-echo "  share                : start sharing files tool"
+echo -e "  ${COLOR_CMD}share${COLOR_DEFAULT}                : start sharing files tool"
 echo -e ${COLOR_PARAM}
 echo " -------------------------------------------------------------- "
 echo " -- PARAMS (script)                                          -- "
 echo " -------------------------------------------------------------- "
 echo -e ${COLOR_DEFAULT}
-echo "  dtop                 : Command top pour docker"
-echo "  status               : Display container / image status"
-echo "  stopall              : Stop all container"
+echo -e "  ${COLOR_CMD}dtop${COLOR_DEFAULT}                 : Command top pour docker"
+echo -e "  ${COLOR_CMD}status${COLOR_DEFAULT}               : Display container / image status"
+echo -e "  ${COLOR_CMD}stopall${COLOR_DEFAULT}              : Stop all container"
 echo " "
 echo " "
 
