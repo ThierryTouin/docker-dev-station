07b09eb8052c28252fd4b430a1803c962fe6e511
Thierry Touin <thierrytouin@gmail.com>
2022-10-11 12:14:58 +0200

    add waiting process for elasticsearch

--- environnement/dxp/liferay/mount/scripts/elastic-conf.sh
+++ environnement/dxp/liferay/mount/scripts/elastic-conf.sh
@@ -1,2 +1,11 @@
+#!/bin/sh 
+echo "#######################################################"
+echo " Waiting for elasticsearch !"
+
+until $(curl --output /dev/null --silent --head --fail http://elasticsearch:9200); do
+    printf '.'
+    sleep 5
+done
+echo "DONE."
 curl -XPUT -H "Content-Type: application/json" http://elasticsearch:9200/_cluster/settings -d '{ "transient": { "cluster.routing.allocation.disk.threshold_enabled": false } }'
 curl -XPUT -H "Content-Type: application/json" http://elasticsearch:9200/_all/_settings -d '{"index.blocks.read_only_allow_delete": null}'
