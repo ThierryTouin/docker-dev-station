c0ee1a370e0712557d49b08fdc38ee8f06e2ddd4
Touin Thierry <thierrytouin@gmail.com>
2022-10-19 16:54:17 +0200

    add monitor script

---
+++ environnement/dxp/liferay/mount/monitor/dirScan.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "#######################################################"
+echo " Scan directories : "
+
+function scanDir() {
+    nbFicDir=`find $1 -type f | wc -l`
+    sizeDir=`du -sh $1`
+    logDate=`date`
+    echo -e "$1: \t$logDate\t$nbFicDir\t$sizeDir"
+}
+
+until false
+do
+    scanDir /opt/liferay/tomcat/temp
+    scanDir /opt/liferay/osgi/state
+    sleep 30
+done
+echo "DONE."
+
+
