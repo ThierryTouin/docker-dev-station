59b36c15474b3b0e06c4e619497cddf33ab93d8f
Thierry Touin <thierrytouin.pro@gmail.com>
2022-12-07 18:56:15 +0100

    up script version

--- environnement/scripts/check-installation.sh
+++ environnement/scripts/check-installation.sh
@@ -12,6 +12,10 @@ echo "*                 Versions                 *"
 echo "********************************************"
 echo -e ${COLOR_TITLE}
 echo "--------------------------------------------"
+echo -e "${COLOR_PARAM}Linux${COLOR_DEFAULT}   : \n$(lsb_release -a)"
+
+echo -e ${COLOR_TITLE}
+echo "--------------------------------------------"
 echo -e "${COLOR_PARAM}node${COLOR_DEFAULT}   : $(node --version)"
 
 echo -e ${COLOR_TITLE}
@@ -20,11 +24,11 @@ echo -e "${COLOR_PARAM}npm${COLOR_DEFAULT}    : $(npm --version)"
 
 echo -e ${COLOR_TITLE}
 echo "--------------------------------------------"
-echo -e "${COLOR_PARAM}java${COLOR_DEFAULT}   : $(java --version)"
+echo -e "${COLOR_PARAM}java${COLOR_DEFAULT}   : \n$(java --version)"
 
 echo -e ${COLOR_TITLE}
 echo "--------------------------------------------"
-echo -e "${COLOR_PARAM}maven${COLOR_DEFAULT}  : $(mvn --v)"
+echo -e "${COLOR_PARAM}maven${COLOR_DEFAULT}  : \n$(mvn --v)"
 
 echo -e ${COLOR_TITLE}
 echo "--------------------------------------------"
@@ -32,5 +36,8 @@ echo -e "${COLOR_PARAM}gradle${COLOR_DEFAULT} : $(gradle --version)"
 
 echo -e ${COLOR_TITLE}
 echo "--------------------------------------------"
-echo -e "${COLOR_PARAM}docker${COLOR_DEFAULT} : $(docker -v)"
+echo -e "${COLOR_PARAM}docker${COLOR_DEFAULT} : \n$(docker version)"
 
+echo -e ${COLOR_TITLE}
+echo "--------------------------------------------"
+echo -e "${COLOR_PARAM}docker-compose${COLOR_DEFAULT} : \n$(docker-compose version)"
