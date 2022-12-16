0ca7f2d74097d4159b90b723b7c049efa1f302d3
Thierry Touin <thierrytouin@gmail.com>
2022-12-16 18:35:44 +0100

    test with postgresql

---
+++ environnement/db/postgresql/config/1 - init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE dbtest;
+GRANT all privileges ON DATABASE dbtest to dbuser; 
+CREATE DATABASE liferay742;
+GRANT all privileges ON DATABASE liferay742 to dbuser; 
--- environnement/db/postgresql/config/init.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE dbtest;
-GRANT all privileges ON DATABASE dbtest to dbuser; 
