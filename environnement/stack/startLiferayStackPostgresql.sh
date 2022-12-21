79efda8305e8e3f7d736fae8b50b9bed7f42c7f4
Thierry Touin <thierrytouin.pro@gmail.com>
2022-12-21 10:03:14 +0100

    update stack

--- environnement/stack/startLiferayStackPostgresql.sh
+++ environnement/stack/startLiferayStackPostgresql.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+WORKDIR=$PWD
+echo WORKDIR: $WORKDIR
+BASEDIR=$(dirname "$0")
+echo BASEDIR: $BASEDIR
+cd $BASEDIR
+
 ./ecmd.sh apm >/dev/null 2>&1 &
 ./ecmd.sh postgresql >/dev/null 2>&1 &
 ./ecmd.sh elastic >/dev/null 2>&1 &
--- environnement/startAdmin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#./ecmd.sh admin >/dev/null 2>&1 &
-#./ecmd.sh logs >/dev/null 2>&1 &
-#./ecmd.sh ui >/dev/null 2>&1 &
-./ecmd.sh admin &
-./ecmd.sh logs &
-./ecmd.sh ui &
