3f73effff57de177648308d84a162da9cd69187d
Thierry Touin <thierrytouin.pro@gmail.com>
2022-12-21 10:07:49 +0100

    update stack

--- environnement/stack/startLiferayStackMysql.sh
+++ environnement/stack/startLiferayStackMysql.sh
@@ -9,6 +9,6 @@ cd $BASEDIR
 ./ecmd.sh apm >/dev/null 2>&1 &
 ./ecmd.sh mysql >/dev/null 2>&1 &
 ./ecmd.sh elastic >/dev/null 2>&1 &
-rm -f ./dxp/liferay/mount/files/tomcat/conf/Catalina/localhost/ROOT.xml
-cp ./dxp/liferay/mount/files/tomcat/conf/Catalina/localhost/ROOT.xml_mysql ./dxp/liferay/mount/files/tomcat/conf/Catalina/localhost/ROOT.xml
+rm -f $BASEDIR/dxp/liferay/mount/files/tomcat/conf/Catalina/localhost/ROOT.xml
+cp $BASEDIR/dxp/liferay/mount/files/tomcat/conf/Catalina/localhost/ROOT.xml_mysql $BASEDIR/dxp/liferay/mount/files/tomcat/conf/Catalina/localhost/ROOT.xml
 ./ecmd.sh liferay >/dev/null 2>&1 &
--- environnement/stack/startLiferayStackPostgresql.sh
+++ environnement/stack/startLiferayStackPostgresql.sh
@@ -9,6 +9,6 @@ cd $BASEDIR
 ./ecmd.sh apm >/dev/null 2>&1 &
 ./ecmd.sh postgresql >/dev/null 2>&1 &
 ./ecmd.sh elastic >/dev/null 2>&1 &
-rm -f ./dxp/liferay/mount/files/tomcat/conf/Catalina/localhost/ROOT.xml
-cp ./dxp/liferay/mount/files/tomcat/conf/Catalina/localhost/ROOT.xml_postgresql ./dxp/liferay/mount/files/tomcat/conf/Catalina/localhost/ROOT.xml
+rm -f $BASEDIR/dxp/liferay/mount/files/tomcat/conf/Catalina/localhost/ROOT.xml
+cp $BASEDIR/dxp/liferay/mount/files/tomcat/conf/Catalina/localhost/ROOT.xml_postgresql $BASEDIR/dxp/liferay/mount/files/tomcat/conf/Catalina/localhost/ROOT.xml
 ./ecmd.sh liferay >/dev/null 2>&1 &
