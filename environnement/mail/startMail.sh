4664e7d5ed2e801b0feabf5e555020471410e387
Thierry Touin <thierrytouin.pro@gmail.com>
2023-10-06 10:36:32 +0200

    add fake-smtp : non testé

--- environnement/mail/startMail.sh
+++ environnement/mail/startMail.sh
@@ -1,5 +1,6 @@
 
 #!/bin/bash
 echo " Starting Mail server"
-docker compose -f ./mockmock-compose.yml up dds_mockmock
-docker compose logs --follow dds_mockmock
+#docker compose -f ./mockmock-compose.yml up dds_mockmock
+docker compose -f ./fake-smtp-compose.yml up dds_fake-email
+docker compose logs --follow dds_fake-email
