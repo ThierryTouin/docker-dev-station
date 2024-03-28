28597956aa5fe6ca47541a523d12911940ba1cc6
Thierry Touin <thierrytouin.pro@gmail.com>
2024-03-28 17:29:02 +0100

    test proxy

--- environnement/proxy/simple/src/proxy.js
+++ environnement/proxy/simple/src/proxy.js
@@ -6,9 +6,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 // Configuration
-const PORT = 3000;
+const PORT = 8001;
 const HOST = "localhost";
 const API_SERVICE_URL = "https://httpbin.org";
+//const API_SERVICE_URL = "https://www.google.com";
 
 /**
  * Configure proxy middleware
@@ -17,6 +18,20 @@ const myProxy = createProxyMiddleware({
     target: API_SERVICE_URL,
     changeOrigin: true, // for vhosted sites, changes host header to match to target's host
     logger: console,
+    //pathFilter: '/info', // proxy-only-this-path
+    // on: {
+    //   proxyReq: (proxyReq, req, res) => {
+    //     console.log('proxyReq');
+    //   },
+    //   proxyRes: (proxyRes, req, res) => {
+    //     console.log('proxyRes');
+    //   },
+    //   error: (err, req, res) => {
+    //     console.log('error');
+    //   },
+    // }
+
+
   });
 
 // Logging
@@ -36,3 +51,5 @@ app.listen(PORT, HOST, () => {
 
 process.on('SIGINT', () => server.close());
 process.on('SIGTERM', () => server.close());
+
+
