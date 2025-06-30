8ab5bfd13755de6b0d4c15532313ae21d5c9109c
thierry <thierrytouin@gmail.com>
2025-06-30 09:24:21 +0000

    up portainer

--- environnement/admin/admin.sh
+++ environnement/admin/admin.sh
@@ -1,6 +1,6 @@
-#!/bin/sh 
+#!/bin/sh
 echo starting portainer...
 docker compose -f ./portainer-compose.yml up -d
 echo "Admin UI started at \e[92mhttp://localhost:9999 \e[0m"
-#docker logs portainer
+docker logs dds-portainer
 
